f9f944d7e5abadc6ad1238991a48b48f347b6232
Sim121 <simonherlin@gmail.com>
2026-04-03 13:06:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    Core : state, auth, sync, navigation, settings, init
    ============================================================ */
 
-const APP_VERSION = '3.2.14';
+const APP_VERSION = '3.2.15';
 const APP_DATE    = '2026-04-03';
 
 // ── STATE GLOBAL ──
